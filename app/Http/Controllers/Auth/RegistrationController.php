11616eed0ac4652f51139848c3822f8dd7985686
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-10 20:26:19 +0100

    Add Registration controller

---
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Store a newly created user with email, name, and password.
+     *
+     * @param Request $request
+     * @return Response
+     * Returns newly registered user information.
+     */
+    public function register(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:3|max:20',
+        ]);
+
+        if($validator->fails()){
+            return response($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password'))]);
+
+        return response([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
+    }
+}
