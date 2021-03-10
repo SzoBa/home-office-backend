5864332652afc230c4b6ecc0ec6086ef2ef1c9aa
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-10 16:18:26 +0100

    Add middleware groups and login, logout routes

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+/**
+ * This version for backend OAuth identification, currently not necessary
+ */
+//Route::get('/auth/google-redirect', function () {
+//    return Socialite::driver('google')->stateless()->redirect();
+//});
+//
+//Route::get('/auth/google-callback', function () {
+//    $user = Socialite::driver('google')->stateless()->user();
+//});
+
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::post('/login', 'Auth\LoginController@login');
 });
 
-Route::get('/auth/redirect', function () {
-    return Socialite::driver('google')->redirect();
-});
 
-Route::get('/auth/callback', function () {
-    $user = Socialite::driver('google')->stateless()->user();
-    //this is the logged in user, and ex. $user->token, etc...getId, getNickname, getName, getEmail, getAvatar,
-    //OAuth2 $user->token, refreshToken, expiresIn
-    //or from the token - $user = Socialite::driver('google')->userFromToken($token);
-    //or $user = Socialite::driver('google')->userFromTokenAndSecret($token, $secret); - OAuth1
-    //Stateless does NOT work with Twitter
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::delete('/logout', 'Auth\LogoutController@logout');
 });
