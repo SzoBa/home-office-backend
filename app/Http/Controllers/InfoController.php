ab7252ed7a5042a3197635a658c5ab7d626470e9
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 15:31:23 +0100

    Add Info controller for information and readme

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InfoController extends Controller
+{
+    public function index() {
+        $path= config("app.readme");
+        $file = base_path($path);
+        return response()->download($file, "Readme.md");
+    }
+}
