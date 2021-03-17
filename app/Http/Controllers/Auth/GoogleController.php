100808849e6b77c421540196cec8d98eacd92ba8
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 01:22:37 +0100

    Add read-only scope to google controller

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -11,8 +11,10 @@ class GoogleController extends Controller
 {
     public function loginUrl()
     {
+        $scopes = ['https://www.googleapis.com/auth/gmail.readonly'];
         return response([
-            'url' => Socialite::driver('google')->stateless()->redirect()->getTargetUrl(),
+            'url' => Socialite::driver('google')
+                ->scopes($scopes)->stateless()->redirect()->getTargetUrl(),
         ]);
     }
 
