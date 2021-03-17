1b956215f4b23a265c4b689bd1d5e303ef2ba73f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 01:22:08 +0100

    Add get all email id and get email by id

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,24 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class EmailController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return mixed
+     * @throws \JsonException
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+        $socialData = $user->socialData[0]; //multiple social data to one user!!!!
+        $handle = curl_init();
+        $url = "https://www.googleapis.com/gmail/v1/users/{$socialData->social_id}/messages";
+        $headers = ['Accept: application/json', 'Content-Type: application/json',
+            "Authorization: Bearer {$socialData->access_token}",
+                    ];
+        curl_setopt_array($handle, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => $headers,
+            ]
+        );
+        $data = curl_exec($handle);
+        curl_close($handle);
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -30,12 +48,29 @@ class EmailController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     * @throws \JsonException
      */
-    public function show($id)
+    public function show(Request $request, int $id): Response
     {
-        //
+        $user = $request->user();
+        $socialData = $user->socialData[0]; //multiple social data to one user!!!!
+        $handle = curl_init();
+        $url = "https://www.googleapis.com/gmail/v1/users/{$socialData->social_id}/messages/{$id}";
+        $headers = ['Accept: application/json', 'Content-Type: application/json',
+            "Authorization: Bearer {$socialData->access_token}",
+        ];
+        curl_setopt_array($handle, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => $headers,
+            ]
+        );
+        $data = curl_exec($handle);
+        curl_close($handle);
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
     }
 
     /**
@@ -43,7 +78,7 @@ class EmailController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -54,7 +89,7 @@ class EmailController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
