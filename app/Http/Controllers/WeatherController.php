9bab714e0341dcbc6b7ac45d4b94eed9c16b4aaa
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-18 16:42:27 +0100

    Add getWeather method

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+
+    public function getWeather(Request $request)
+    {
+        /** Budapest by default */
+        $latitude = $request->input("latitude", 47.497913);
+        $longitude = $request->input("longitude", 19.040236);
+        $weatherApiKey = env('OPENWEATHER_APIKEY');
+
+        $handle = curl_init();
+        $url = "http://api.openweathermap.org/data/2.5/weather?lat={$latitude}&lon={$longitude}&appid={$weatherApiKey}";
+        $headers = ['Accept: application/json', 'Content-Type: application/json'];
+        curl_setopt_array($handle, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => $headers,
+            ]
+        );
+        $data = curl_exec($handle);
+        curl_close($handle);
+        return response($data, 200);
+    }
+}
