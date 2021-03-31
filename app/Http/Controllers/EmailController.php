f9e437147677762aba9634a518218675c40d7d5f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-01 00:56:06 +0200

    Refactor EmailController

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -104,38 +104,4 @@ class EmailController extends Controller
         //
     }
 
-
-    public function mailOptions(Request $request)
-    {
-        $user = $request->user();
-        $socialData = $user->socialData->where('social_type', 'google')->first();
-        $queryParams = $request->input('q');
-        $handle = curl_init();
-        $baseUrl = config("app.gmailApiUrl") . "/{$socialData->social_id}/messages";
-        $url = $queryParams ? $baseUrl . "?q=" . str_replace(" ", "+", $queryParams) : $baseUrl;
-        $headers = [
-            'Accept: application/json',
-            'Content-Type: application/json',
-            "Authorization: Bearer {$socialData->access_token}",
-        ];
-        curl_setopt_array(
-            $handle,
-            [
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_HTTPHEADER => $headers,
-            ]
-        );
-        $data = curl_exec($handle);
-        curl_close($handle);
-        $jsonData = json_decode($data, false, 512, JSON_THROW_ON_ERROR);
-        if (isset($jsonData->error)) {
-            if ($jsonData->error->status === "UNAUTHENTICATED") {
-                return \response("Login with Google to access mails!", 422);
-            }
-            return \response($jsonData->error->message, 400);
-        }
-        return response($data, 200);
-    }
-
 }
